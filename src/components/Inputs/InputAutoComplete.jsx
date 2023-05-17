150a0b7e34d9032891d975b6f01c38bf878097d4
Jose Riquelme <joseriquelme22@gmail.com>
2023-05-17 10:41:11 -0400

    animations modals

--- src/components/Inputs/InputAutoComplete.jsx
+++ src/components/Inputs/InputAutoComplete.jsx
@@ -1,67 +1,67 @@
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 const InputAutocomplete = ({ value, options, onSelect, placeholder, disabled = false, formSubmit = false }) => {
-	const [inputValue, setInputValue] = useState('');
-	const uniqueOptions = [...new Set(options)];
+  const [inputValue, setInputValue] = useState('');
+  const uniqueOptions = [...new Set(options)];
 
-	return (
-		<>
-			<Autocomplete
-				style={{ fontSize: '46px' }}
-				disabled={disabled}
-				value={value}
-				onChange={(event, newValue) => {
-					onSelect(newValue);
-				}}
-				inputValue={inputValue}
-				onInputChange={(event, newInputValue) => {
-					setInputValue(newInputValue);
-				}}
-				options={uniqueOptions}
-				noOptionsText='No se encontraron coincidencias'
-				sx={{
-					'& .MuiInputBase-root': {
-						border: `1px solid ${formSubmit && !value ? '#EF4444' : '#E5E7EB'}`,
-						borderRadius: '0.375rem',
-						backgroundColor: '#e2e8f0',
-						'&:hover': {
-							borderColor: disabled ? '#E5E7EB' : '#0EA5E9',
-						},
-						'&.Mui-focused': {
-							backgroundColor: '#e2e8f0',
-							borderColor: '#0EA5E9',
-						},
-					},
-					'& .MuiAutocomplete-listbox li': {
-						'&:hover': {
-							backgroundColor: disabled ? '#fff' : '#E5E7EB',
-							cursor: disabled ? 'default' : 'pointer',
-						},
-					},
-					'& .MuiInputBase-input': {
-						fontSize: '14px',
-						color: '#111827',
-					},
-				}}
-				renderInput={(params) => (
-					<TextField
-						{...params}
-						variant='standard'
-						placeholder={placeholder}
-						InputProps={{
-							...params.InputProps,
-							disableUnderline: true,
-							classes: {
-								root: 'pl-4 p-2 shadow-sm',
-							},
-						}}
-					/>
-				)}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Autocomplete
+        style={{ fontSize: '46px' }}
+        disabled={disabled}
+        value={value}
+        onChange={(event, newValue) => {
+          onSelect(newValue);
+        }}
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          setInputValue(newInputValue);
+        }}
+        options={uniqueOptions}
+        noOptionsText='No se encontraron coincidencias'
+        sx={{
+          '& .MuiInputBase-root': {
+            border: `1px solid ${formSubmit && !value ? '#EF4444' : '#E5E7EB'}`,
+            borderRadius: '0.375rem',
+            backgroundColor: '#e2e8f0',
+            '&:hover': {
+              borderColor: disabled ? '#E5E7EB' : '#0EA5E9',
+            },
+            '&.Mui-focused': {
+              backgroundColor: '#e2e8f0',
+              borderColor: '#0EA5E9',
+            },
+          },
+          '& .MuiAutocomplete-listbox li': {
+            '&:hover': {
+              backgroundColor: disabled ? '#fff' : '#E5E7EB',
+              cursor: disabled ? 'default' : 'pointer',
+            },
+          },
+          '& .MuiInputBase-input': {
+            fontSize: '14px',
+            color: '#111827',
+          },
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant='standard'
+            placeholder={placeholder}
+            InputProps={{
+              ...params.InputProps,
+              disableUnderline: true,
+              classes: {
+                root: 'pl-4 p-2 shadow-sm',
+              },
+            }}
+          />
+        )}
+      />
+    </>
+  );
 };
 
 export default InputAutocomplete;
--- src/components/Modals/ModalLogout.jsx
+++ src/components/Modals/ModalLogout.jsx
@@ -2,50 +2,50 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const ModalLogout = () => {
-	const navigate = useNavigate();
-	const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
-	const handleToggleModal = (shouldClose) => {
-		setOpen(!shouldClose);
-	};
+  const handleToggleModal = (shouldClose) => {
+    setOpen(!shouldClose);
+  };
 
-	const handleLogout = () => {
-		localStorage.clear();
-		navigate('/');
-		setOpen(false);
-	};
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/');
+    setOpen(false);
+  };
 
-	return (
-		<div>
-			<button
-				id='singOff'
-				className='text-xs sm:text-base text-white'
-				onClick={() => handleToggleModal(false)}></button>
-			{open && (
-				<div
-					className='fixed inset-0 z-20 bg-black bg-opacity-50 flex items-center justify-center'
-					onClick={() => handleToggleModal(true)}>
-					<div className='bg-white rounded-lg w-96 p-4 flex flex-col items-center border border-lime-400 bg-opacity-90 mx-2'>
-						<h2 className='text-base font-medium mb-4'>¿Estás seguro de cerrar sesión?</h2>
-						<div className='flex space-x-4'>
-							<button
-								className='bg-slate-200 hover:bg-slate-300 transition-colors duration-300
-                                         text-gray-900 rounded-lg px-2 py-1 text-sm md:text-base border'
-								onClick={() => handleToggleModal(true)}>
-								Cancelar
-							</button>
-							<button
-								className='bg-pink-600 hover:bg-pink-700 transition-colors duration-300
-                                         text-slate-100 rounded-lg px-2 py-1 text-sm md:text-base'
-								onClick={handleLogout}>
-								Confirmar
-							</button>
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div>
+      <button
+        id='singOff'
+        className='text-xs text-white sm:text-base'
+        onClick={() => handleToggleModal(false)}></button>
+      {open && (
+        <div
+          className='fade-in fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50'
+          onClick={() => handleToggleModal(true)}>
+          <div className='scale-in-top mx-2 flex w-96 flex-col items-center rounded-lg border border-lime-400 bg-white bg-opacity-90 p-4'>
+            <h2 className='mb-4 text-base font-medium'>¿Estás seguro de cerrar sesión?</h2>
+            <div className='flex space-x-4'>
+              <button
+                className='rounded-lg border bg-slate-200 px-2
+                           py-1 text-sm text-gray-900 transition-colors duration-300 hover:bg-slate-300 md:text-base'
+                onClick={() => handleToggleModal(true)}>
+                Cancelar
+              </button>
+              <button
+                className='rounded-lg bg-pink-600 px-2 py-1
+                           text-sm text-slate-100 transition-colors duration-300 hover:bg-pink-700 md:text-base'
+                onClick={handleLogout}>
+                Confirmar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ModalLogout;
